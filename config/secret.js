88ce33a82373fa7a04b321a318a78365fcad612b
madhusudhanj7 <madhusudhanj7@gmail.com>
2021-05-10 21:02:49 +0530

    first commit after cloning

---
+++ config/secret.js
@@ -0,0 +1,20 @@
+// const secret = {
+//     port: 3000,
+//     secret: "testing",
+//     database: "mongodb://localhost:27017/GigzApp",
+//     algolia_appId: "H3KEKXEIM1", // You can get it from this link https://www.algolia.com/users/sign_in
+//     algolia_api_key: "bb9d548ad94925bab6402392c290aebb",
+//     }
+    
+//     module.exports = secret
+
+
+    const secret = {
+        port: 3000,
+        secret: "testing",
+        database: "mongodb://localhost:27017/",
+        algolia_appId: "H3KEKXEIM1",
+        algolia_api_key: "21d5eeeac262264e0c790503820603f5",
+      }
+
+      module.exports = secret
